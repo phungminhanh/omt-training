64b57b6428f83fb59c669e808d86a4e446bdb3d4
phunganh23082 <phunganh23082k@gmail.com>
2024-01-11 10:53:08 +0700

    Initial commit

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-// Function to get weather information based on user's location
+
 async function getWeatherByLocation() {
     try {
-      // Get the user's location using Geolocation API
+      
       navigator.geolocation.getCurrentPosition(async (position) => {
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
   
-        // Use fetch to get weather data from OpenWeatherMap
-        const apiKey = '9ffbc24db6da404118e93cdc0a9db285'; // Your OpenWeatherMap API key
+       
+        const apiKey = '9ffbc24db6da404118e93cdc0a9db285'; 
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`);
         const data = await response.json();
   
         const cityName = data.name;
-        const temperature = Math.round(data.main.temp - 273.15); // Convert Kelvin to Celsius
+        const temperature = Math.round(data.main.temp - 273.15); 
   
-        // Display weather information on the website
         document.getElementById("city").innerHTML = ` ${cityName}`;
         document.getElementById("temperature").innerHTML = `${temperature}°C`;
         document.getElementById("weather-icon").src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
   
-        // Update date and time
+     
         updateDateTime();
       });
     } catch (error) {
@@ -28,26 +27,26 @@ async function getWeatherByLocation() {
     }
   }
   
-  // Function to update date and time
+
   function updateDateTime() {
     const currentDate = new Date();
     const formattedDate = currentDate.toLocaleDateString();
     document.getElementById("date").innerHTML = `${formattedDate}`;
   }
   
-  // Call the function to get weather by location when the website is loaded
+  
   getWeatherByLocation();
-  ///
- // on document ready
+
+
 $(document).ready(function() {
 
-  // show/hide the mobile menu based on class added to container
+
   $('.menu-icon').click(function() {
       $(this).parent().toggleClass('is-tapped');
       $('#hamburger').toggleClass('open');
   });
 
-  // handle touch device events on drop down, first tap adds class, second navigates
+ 
   $('.touch .sitenavigation li.nav-dropdown > a').on('touchend',
       function(e) {
           if ($('.menu-icon').is(':hidden')) {
@@ -67,7 +66,7 @@ $(document).ready(function() {
           }
       });
 
-  // handle the expansion of mobile menu drop down nesting
+  
   $('.sitenavigation li.nav-dropdown').click(
       function(event) {
           if (event.stopPropagation) {
@@ -83,7 +82,7 @@ $(document).ready(function() {
       }
   );
 
-  // prevent links for propagating click/tap events that may trigger hiding/unhiding
+  
   $('.sitenavigation a.nav-dropdown, .sitenavigation li.nav-dropdown a').click(
       function(event) {
           if (event.stopPropagation) {
@@ -94,7 +93,7 @@ $(document).ready(function() {
       }
   );
 
-  // javascript fade in and out of dropdown menu
+ 
   $('.no-touch .sitenavigation li').hover(
       function() {
           if (!$('.menu-icon').is(':visible')) {
